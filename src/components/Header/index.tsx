f5ba3a7f0c64fdb31546e230f48bde9083ebe347
Fernando Souza <l.fernando.dev@gmail.com>
2023-07-09 12:42:01 -0300

    finished styles the app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderConntent, HeaderContainer, NewTrasactionButton } from './styles';
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderConntent>
         <img src={logoImg} alt="Logo DT Money" />
-        <NewTrasactionButton>Nova Transação</NewTrasactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTrasactionButton>Nova Transação</NewTrasactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+        </Dialog.Root>
       </HeaderConntent>
     </HeaderContainer>
   );
