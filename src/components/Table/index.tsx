6221d37bd1a43dfaed376428d0435a31a92e382b
Fernando Souza <l.fernando.dev@gmail.com>
2023-07-10 23:06:48 -0300

    add context and formatted price and date

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,29 +1,30 @@
+import { useContext } from "react";
 import { PriceHighlight, TableContainer } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Table() {
+
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <TableContainer>
       <tbody>
-        <tr>
-          <td width='50%'>Desenvolvimento de site</td>
-          <td>
-            <PriceHighlight variant="income">
-              R$ 12.000,00
-            </PriceHighlight>
-          </td>
-          <td>Venda</td>
-          <td>13/04/2022</td>
-        </tr>
-        <tr>
-          <td width='50%'>Hamburger</td>
-          <td>
-            <PriceHighlight variant="outcome">
-              - R$ 59,00
-            </PriceHighlight>
-          </td>
-          <td>Saída</td>
-          <td>10/04/2022</td>
-        </tr>
+        {transactions.map(transaction => {
+          return (
+            <tr key={transaction.id}>
+              <td width='50%'>{transaction.description}</td>
+              <td>
+                <PriceHighlight variant={transaction.type}>
+                  {transaction.type === 'outcome' && '- '}
+                  {priceFormatter.format(transaction.price)}
+                </PriceHighlight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+            </tr>
+          )
+        })}
       </tbody>
     </TableContainer>
   );
